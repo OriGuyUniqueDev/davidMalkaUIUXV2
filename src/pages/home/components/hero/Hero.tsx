0118cccd5bb0d11de1088940786780cdd34c5b8c
OriGuyUniqueDev <origuydev@gmail.com>
2023-02-03 18:21:18 +0200

    remove unused div

--- src/pages/home/components/hero/Hero.tsx
+++ src/pages/home/components/hero/Hero.tsx
@@ -8,11 +8,7 @@ const Hero: FunctionComponent<HeroProps> = () => {
 	return (
 		<div className="w-full h-[81.2rem]   bg-projectBlack x-28">
 			<div className="container mx-auto">
-				<div className="w-full mx-auto">
-					<LazyLoadImage src="images/homePage/hero.png" alt="hero img" width="100%" className=" w-[112.3rem] mx-auto h-[65.2rem] pt-[4.2rem] relative" placeholderSrc={"loading..."} effect="blur" />
-				</div>
-				{/* <img className=" w-[112.3rem] mx-auto h-[65.2rem] pt-[4.2rem] relative" src="images/homePage/hero.png" alt="hero img" /> */}
-
+				<LazyLoadImage src="images/homePage/hero.png" alt="hero img" width="100%" className=" w-[112.3rem] mx-auto h-[65.2rem] pt-[4.2rem] relative" placeholderSrc={"loading..."} effect="blur" />
 				<p className="relative bottom-[32rem] left-20 w-[60rem] font-serif text-white text-[6.4rem] leading-[6.97rem] ">
 					Hi, I’m David UI/UX, Product designer, <span className="text-[2.4rem]">(and team leader)</span>
 				</p>
