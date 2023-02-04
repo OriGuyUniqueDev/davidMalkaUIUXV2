1c90f1e9bef9309f1ea8f033a3f070e59238152a
OriGuyUniqueDev <origuydev@gmail.com>
2023-02-04 13:29:27 +0200

    add about page

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -16,7 +16,7 @@ const Header: FunctionComponent<HeaderProps> = () => {
 		textDecoration: "underline",
 	};
 	return (
-		<div className="w-full mx-auto bg-projectBlack md:h-[6rem] sm:h-[10rem] sm:mb-[2.4rem]     ">
+		<div className="w-full mx-auto bg-projectBlack md:h-[6rem] sm:h-[10rem] sm:mb-[2.4rem] md:mb-0     ">
 			<div className="md:max-w-[1280px] sm:max-w-[375px] mx-auto h-full md:px-[7rem] sm:px-[1.8rem] flex justify-between md:items-center sm:items-end">
 				<Link to="/">
 					<img src="/icons/logoLight.png" alt="logo in nav bar" />
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -4,12 +4,22 @@ interface AboutProps {}
 
 const About: FunctionComponent<AboutProps> = () => {
 	return (
-		<div className="">
-			<div className="flex gap-8">
-				<img src="/images/homePage/avatarContact.webp" alt="avatarContact" />
+		<div className="bg-projectWhite px-[7rem] py-[12rem]">
+			<div className="flex gap-[2.4rem] items-center">
+				<img className="w-[20rem] h-[20rem]" src="/images/homePage/avatarContact.webp" alt="avatarContact" />
 				<p className="text-[8.4rem] font-serif w-[52rem] font-bold">A little story about me...</p>
 			</div>
-			<p className="text-[1.8rem] text-projectGrey">Hi there! I'm David, a passionate and experienced UI/UX and product designer with 5 years of experience under my belt, I bring a wealth of creative abilities and a diverse set of design skills to the table. I hold a certificate in digital UI/UX studies from Tiltan College and I'm currently leading the design studio at Abra Web & Mobile. I'm proud of my knowledge and experience in UI/UX design, which allows me to confidently tackle any project and bring my unique touch to it. My goal is always to create designs with unbeatable usability and design, and I'm always up for a new challenge in my career. As the head of the design studio, I'm responsible for guiding a team of talented designers. I love working with my team to bring the best possible user experience to our clients, and I'm always looking for ways to improve and streamline our processes. My passion for design drives me forward, and my goal is to become a senior UX/UX designer, leading projects and making a real difference in the industry.</p>
+			<div className="w-[61.7rem] ml-[23rem] mt-[3.4rem] text-[1.8rem] flex flex-col gap-[2.8rem]">
+				<p className=" text-projectGrey">
+					<p className=" text-projectGrey">Hi there!</p>I'm David, a passionate and experienced UI/UX and product designer with 5 years of experience under my belt, I bring a wealth of creative abilities and a diverse set of design skills to the table. I hold a certificate in digital UI/UX studies from Tiltan College and I'm currently leading the design studio at Abra Web & Mobile.
+				</p>
+				<p className=" text-projectGrey">I'm proud of my knowledge and experience in UI/UX design, which allows me to confidently tackle any project and bring my unique touch to it. My goal is always to create designs with unbeatable usability and design, and I'm always up for a new challenge in my career.</p>
+				<p className=" text-projectGrey"> As the head of the design studio, I'm responsible for guiding a team of talented designers. I love working with my team to bring the best possible user experience to our clients, and I'm always looking for ways to improve and streamline our processes.</p>
+				<p className=" text-projectGrey"> My passion for design drives me forward, and my goal is to become a senior UX/UX designer, leading projects and making a real difference in the industry.</p>
+				<a href={``} className={`px-6 py-7 mt-[4.6rem] md:w-[16.9rem] sm:w-full h-[6.1rem] flex items-center gap-4 justify-center border-projectBlack   border-[0.2rem] font-bold text-2xl`}>
+					Download CV <img src="/icons/download.svg" alt="download icon" />
+				</a>
+			</div>
 		</div>
 	);
 };
