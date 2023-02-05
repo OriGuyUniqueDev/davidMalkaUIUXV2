a741e7d42f0f98a1f229b33fe10fb6722b64c6a5
OriGuyUniqueDev <origuydev@gmail.com>
2023-02-05 18:05:42 +0200

    ramAderet hires

--- src/pages/home/components/hero/Hero.tsx
+++ src/pages/home/components/hero/Hero.tsx
@@ -7,17 +7,17 @@ interface HeroProps {}
 const Hero: FunctionComponent<HeroProps> = () => {
 	const [imageURL, setImageURL] = useState("");
 	window.addEventListener("resize", () => {
-		window.innerWidth <= 1280 ? setImageURL("/images/homePage/heroMobile.png") : setImageURL("/images/homePage/hero.webp");
+		window.innerWidth <= 1279 ? setImageURL("/images/homePage/heroMobile.png") : setImageURL("/images/homePage/hero.webp");
 	});
 	useEffect(() => {
-		window.innerWidth <= 1280 ? setImageURL("/images/homePage/heroMobile.png") : setImageURL("/images/homePage/hero.webp");
+		window.innerWidth <= 1279 ? setImageURL("/images/homePage/heroMobile.png") : setImageURL("/images/homePage/hero.webp");
 	}, []);
 	return (
 		<div className="w-full md:h-[81.2rem]  bg-projectBlack ">
 			<div className="container mx-auto">
 				<LazyLoadImage src={imageURL} alt="hero img" width="100%" className=" md:px-[7rem] sm:px-[1.8rem]  mx-auto h-[65.2rem] pt-[4.2rem] relative" effect="opacity" />
 
-				<p className="relative sm:bottom-[20rem] md:bottom-[30.4rem] left-20 md:w-[60rem] sm:w-[36rem] font-serif text-white md:text-[6.4rem] font-bold sm:text-[3.6rem] md:leading-[6.97rem] sm:leading-[4.17rem] ">
+				<p className="relative sm:bottom-[20rem] md:bottom-[30.4rem] left-20 md:w-[60rem] sm:w-[30rem] font-serif text-white md:text-[6.4rem] font-bold sm:text-[3.6rem] md:leading-[6.97rem] sm:leading-[4.17rem] ">
 					Hi, I’m David UI/UX, Product designer, <span className="text-[2.4rem] font-normal block">(and team leader)</span>
 				</p>
 			</div>
--- src/pages/projectPage/components/AboutProject.tsx
+++ src/pages/projectPage/components/AboutProject.tsx
@@ -8,10 +8,10 @@ const AboutProject: FunctionComponent<AboutProjectProps> = ({ data }) => {
 	return (
 		<div className="bg-projectWhite w-full md:py-[12rem] sm:py-[6rem] ">
 			<div className="md:max-w-[1280px] mx-auto md:px-[7rem] sm:px-[1.8rem] ">
-				<div className="w-[55.6rem]">
+				<div className="w-full">
 					<p className="text-[2.1rem] uppercase sm:hidden md:block   text-projectGrey">Introduction.</p>
 					<h1 className="font-serif w-[41.9rem] md:text-[8.4rem] text-projectBlack  sm:text-[4.8rem] font-bold mb-[2.4rem]">About the project</h1>
-					<p className="text-[1.8rem] sm:w-[34.2rem]">{data}</p>
+					<p className="text-[1.8rem] md:w-[67.8rem] sm:w-[34.2rem]">{data}</p>
 				</div>
 			</div>
 		</div>
