8a94125d474e5e7f7d68f6311cd747f688a94335
OriGuyUniqueDev <origuydev@gmail.com>
2023-02-07 21:03:39 +0200

    fix gero section

--- src/pages/projectPage/components/HeroProject.tsx
+++ src/pages/projectPage/components/HeroProject.tsx
@@ -17,7 +17,7 @@ const HeroProject: FunctionComponent<HeroProjectProps> = ({ imageBG, image, resI
 		window.innerWidth <= 1280 ? setIsMobile(true) : setIsMobile(false);
 	}, []);
 	return (
-		<div className="w-full md:h-[52.5rem] sm:h-[42.5rem]  bg-projectBlack ">
+		<div className="w-full md:h-[52.5rem] sm:h-[42.5rem] relative z-30 bg-projectBlack ">
 			<div className="container mx-auto">
 				<LazyLoadImage src={imageBG.url} alt={imageBG.alt} width="100%" className=" md:px-[7rem] sm:px-[1.8rem]  mx-auto md:h-[44.1rem] sm:h-[36rem]  md:relative  pt-[4.2rem] relative" effect="opacity" />
 				<LazyLoadImage src={image.url} alt={image.alt} className="h-[44.4rem] 	 md:px-[7rem] sm:px-[1.8rem]  mx-auto    pt-[4.2rem] relative bottom-[28.5rem] md:left-[45rem] sm:left-[0rem]" effect="opacity" />
